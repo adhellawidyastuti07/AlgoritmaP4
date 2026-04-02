35424c478b4ee347b16e53249af3108626b9177d
adhellawidyastuti07 <adhellawidyastuti8@gmail.com>
2026-04-02 19:13:45 +0700

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+batas = int(input("\nMasukkan batas Angka (1-100) : "))
+
+print("\n===== FizzBuzz =====")
+for i in range(1, batas + 1):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i) 
